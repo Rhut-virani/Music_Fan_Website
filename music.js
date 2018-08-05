fc100f9e1745f27da4fa5b4e393c50d949fd2658
Rhut-virani <rhut.virani@gmail.com>
2018-08-05 10:58:11 -0400

    Changing Folder Structure

---
+++ music.js
@@ -0,0 +1,88 @@
+$( ".img-1, .imgt-1" ).hover(
+    function() {
+      $( "body" ).addClass( "bimg-1" );
+    }, function() {
+      $( "body" ).removeClass( "bimg-1" );
+    }
+  );
+
+  $( ".img-2, .imgt-2" ).hover(
+    function() {
+      $( "body" ).addClass( "bimg-2" );
+    }, function() {
+      $( "body" ).removeClass( "bimg-2" );
+    }
+  ); 
+  
+  $( ".img-3, .imgt-3" ).hover(
+    function() {
+      $( "body" ).addClass( "bimg-3" );
+    }, function() {
+      $( "body" ).removeClass( "bimg-3" );
+    }
+  ); 
+  
+  $( ".img-4, .imgt-4" ).hover(
+    function() {
+      $( "body" ).addClass( "bimg-4" );
+    }, function() {
+      $( "body" ).removeClass( "bimg-4" );
+    }); 
+  
+  $( ".img-5, .imgt-5" ).hover(
+    function() {
+      $( "body" ).addClass( "bimg-5" );
+    }, function() {
+      $( "body" ).removeClass( "bimg-5" );
+    }); 
+  
+  $( ".img-6, .imgt-6" ).hover(
+    function() {
+      $( "body" ).addClass( "bimg-6" );
+    }, function() {
+      $( "body" ).removeClass( "bimg-6" );
+    }
+  ); 
+  $( ".img-7, .imgt-7" ).hover(
+    function() {
+      $( "body" ).addClass( "bimg-7" );
+    }, function() {
+      $( "body" ).removeClass( "bimg-7" );
+    }); 
+  
+  $( ".img-8, .imgt-8" ).hover(
+    function() {
+      $( "body" ).addClass( "bimg-8" );
+    }, function() {
+      $( "body" ).removeClass( "bimg-8" );
+    }); 
+  
+  $( ".img-9, .imgt-9" ).hover(
+    function() {
+      $( "body" ).addClass( "bimg-9" );
+    }, function() {
+      $( "body" ).removeClass( "bimg-9" );
+    } ); 
+  
+  $( ".img-10, .imgt-10" ).hover(
+    function() {
+      $( "body" ).addClass( "bimg-10" );
+    }, function() {
+      $( "body" ).removeClass( "bimg-10" );
+    }); 
+  
+  $( ".img-11, .imgt-11" ).hover(
+    function() {
+      $( "body" ).addClass( "bimg-11" );
+    }, function() {
+      $( "body" ).removeClass( "bimg-11" );
+    }
+  );
+
+  $( ".img-12, .imgt-12" ).hover(
+    function() {
+      $( "body" ).addClass( "bimg-12" );
+    }, function() {
+      $( "body" ).removeClass( "bimg-12" );
+    }
+  );
